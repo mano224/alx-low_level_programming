b47cec685be25a79f7252e59c93fbe741a02bc8a
Mano224 <mano@example.com>
2023-09-10 04:53:44 -0700

    h

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * description: print all alphabet letters except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0 ; d <= 9 ; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+}
