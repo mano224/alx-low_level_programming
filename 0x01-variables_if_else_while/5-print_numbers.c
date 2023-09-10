fe420d16a11c8b5b240743238b93f2302a8db8ea
Mano224 <mano@example.com>
2023-09-10 04:14:02 -0700

    j

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * description: print all alphabet letters except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0; d <= 10; d++)
+	{
+		putchar(d + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
