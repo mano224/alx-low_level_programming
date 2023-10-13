0583cc29e7d5ba641ee1c0030e8f97320cb59070
Mano224 <mano@example.com>
2023-10-13 10:08:24 -0700

    hh

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prin numbers from 0 to 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+	
+	for (i = 0 ; i < 10 ; i++)
+	{
+		_putchar(i + 48);
+		_putchar('\n');
+	}
+	return (0);
+}
