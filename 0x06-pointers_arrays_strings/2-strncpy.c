bdfc9b7eada03cbda3e019e05022185113315682
Mano224 <mano@example.com>
2023-10-21 10:06:46 -0700

    j

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,8 +13,11 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (x = 0; x < n && src[x] != '\0'; x++)
 		dest[x] = src[x];
-	for (x < n; dest[x] = '\0'; x++)
-		;
+	while (x < n)
+	{
+		dest[x] = '\0';
+		x++;
+	}
 
 		return (dest);
 }
