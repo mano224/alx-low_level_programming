e9725dd4da8cada0aa50f0dc74f919a42f58f57f
Mano224 <mano@example.com>
2023-10-21 09:41:58 -0700

    j

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,10 +10,10 @@ char *_strcat(char *dest, char *src)
 {
 	int x, x2;
 
-	for (x = 0; dest[x] != '\0'; x++)
+	for (x = 0; dest[x]; x++)
 		;
-	for (x2 = 0; src[x2] != '\0'; x2++)
-		dest[x] = src[x2];
-	x++;
+	for (x2 = 0; src[x2]; x2++)
+		dest[x++] = src[x2];
+
 	return (dest);
 }
