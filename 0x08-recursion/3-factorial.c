26a4a9aae6365465a664d14c711b842c2d367914
Mano224 <mano@example.com>
2023-10-27 09:45:47 -0700

    j

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - finds factorial
+ * @s: int
+ * Return: int
+ */
+int factorial(int n)
+{
+if (n < 0)
+{
+	return (-1);
+}
+else id (n == 0)
+{
+	return (1);
+}
+return (n * factorial(n - 1);
+}
