97578c0abdbea273f9cf4b08cbda7a592e5b3ceb
Mano224 <mano@example.com>
2023-11-01 12:53:37 -0700

    j

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,9 +11,8 @@ char *str_concat(char *s1, char *s2)
 	char *conct;
 	int i, ci;
 
-	if (s1 == NULL)
+	if (s1 == NULL || s2 == NULL)
 		s1 = "";
-	if (s2 == NULL)
 		s2 = "";
 		i = ci = 0;
 	while (s1[i] != '\0')
